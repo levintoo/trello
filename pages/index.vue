eea3c42ddb4d80059c1e709352b0dc6090b90b41
levintoo <tooklevin@gmail.com>
2024-02-17 14:28:49 +0300

    create auth pages

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+<template>
+  <div>
+    <h1 class="mb-4 text-xl font-bold">Welcome {user}</h1>
+    <p>Access your dashboard and account from here</p>
+
+    <button
+      type="button"
+      class="bg-red-500 hover:bg-red-600 transition-all duration-200 text-red-50 rounded-lg py-2 px-5 mt-4"
+    >
+      Logout
+    </button>
+  </div>
+</template>
+
+<style></style>
