ea8a752a7727776d3cf2dc347cb36dea0e1a95b1
levintoo <tooklevin@gmail.com>
2024-02-24 19:48:07 +0300

    design sidebar

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts">
-
+<script setup>
+const darkMode = ref(false)
 </script>
 
 <template>
-  <h1 class="text-gray-800">Yeet app</h1>
+  <main :class="{ 'dark' : darkMode }">
+    <div class="min-h-full bg-white dark:bg-dim-900">
+      <div class="grid grid-cols-12 sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-4">
+
+        <!-- left sidebar -->
+        <div class="hidden md:block xs-col-span-1 xl:colspan-2">
+          <div class="sticky top-0">
+            <SidebarLeft />
+          </div>
+        </div>
+        <!-- main content -->
+        <div class="bg-cyan-100">s</div>
+        <!-- right sidebar -->
+        <div class="bg-purple-100">d</div>
+
+      </div>
+    </div>
+  </main>
 </template>
